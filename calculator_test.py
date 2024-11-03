b1fd3c281f54b9275d1f2ff34689fc5710290bc6
Artem Borisov <artemb.ggw@gmail.com>
2024-11-03 21:53:38 +0300

    Dumb error

--- calculator_test.py
+++ calculator_test.py
@@ -3,4 +3,4 @@ from calculator import calculate
 
 def test_calculate_sub_add():  
     assert calculate(1, 2, '+') == 3  
-    assert calculate_area_square(2, 1, '-') == 1 
+    assert calculate(2, 1, '-') == 1 
