68b29f07e119032789e9b82045bae69ac8efe499
Artem Borisov <artemb.ggw@gmail.com>
2024-11-03 21:11:25 +0300

    Refactoring

--- calculator_test.py
+++ calculator_test.py
@@ -1,6 +1,6 @@
 import pytest
 from calculator import calculate
 
-def test_calculate_area_square():  
+def test_calculate_sub_add():  
     assert calculate(1, 2, '+') == 3  
     assert calculate_area_square(2, 1, '-')) == 1 
